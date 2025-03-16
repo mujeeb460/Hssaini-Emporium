1ff3a7e3d77686e457da4860d2f10f45849f5f33
Rana Mujeeb Rahman <93813491+mujeeb460@users.noreply.github.com>
2025-03-16 06:28:34 +0500

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -29,9 +29,8 @@
                 <div class="footer__widget">
                     <h6>Join Our Newsletter Now</h6>
                     <p>Get E-mail updates about our latest shop and special offers.</p>
-                    <form action="{{ route('subscribe_newsletter')}}" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <input type="text" placeholder="Enter your mail" name="email">
+                    <form action="#">
+                        <input type="text" placeholder="Enter your mail">
                         <button type="submit" class="site-btn">Subscribe</button>
                     </form>
                 </div>
